9e373994c36fad87d9d622d0bad48bf3fa6999e6
ravikantpal <ravikantpal@ravikantpals-MacBook-Pro.local>
2025-10-07 23:47:30 +0530

    Add .gitignore and initial project files

---
+++ register.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Welcome to Savi-plex</title>
+    <link rel="stylesheet" type="text/css" href="asset/style/styles.css">
+</head>
+
+<body>
+    <div class="signInContainer">
+        <div class="column">
+
+        </div>
+    </div>
+
+</body>
+
+</html>
