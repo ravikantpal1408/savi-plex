27dc85e3418db00e1993d5d05e3106d54c9ee3aa
ravikantpal <ravikantpal@ravikantpals-MacBook-Pro.local>
2025-10-11 07:46:20 +0530

    done with db setup

--- register.php
+++ register.php
@@ -1,14 +1,19 @@
 <?php
+require_once("includes/config.php");
 require_once("includes/classes/FormSanitizer.php");
+
+
 	if (isset($_POST["submitButton"])) {
 		$firstName = FormSanitizer::sanitizeFormString($_POST["firstName"]);
 		$lastName = FormSanitizer::sanitizeFormString($_POST["lastName"]);	
-		$email = FormSanitizer::sanitizeFormString($_POST["email"]);
-		$confirmEmail = FormSanitizer::sanitizeFormString($_POST["confirmEmail"]);
-		$password = FormSanitizer::sanitizeFormString($_POST["password"]);
-		$confirmPassword = FormSanitizer::sanitizeFormString($_POST["confirmPassword"]);
+		$username = FormSanitizer::sanitizeFormUsername($_POST["username"]);
+		$email = FormSanitizer::sanitizeFormEmail($_POST["email"]);		
+		$confirmEmail = FormSanitizer::sanitizeFormEmail($_POST["confirmEmail"]);
+		$password = FormSanitizer::sanitizeFormPassword($_POST["password"]);
+		$confirmPassword = FormSanitizer::sanitizeFormPassword($_POST["confirmPassword"]);
 		echo ("Form submitted successfully!");
 	}
+	
 ?>
 <!DOCTYPE html>
 <html>
@@ -30,11 +35,11 @@ require_once("includes/classes/FormSanitizer.php");
 			<form method="POST">
 				<input type="text" name="firstName" id="firstName" placeholder="First Name" required>
 				<input type="text" name="lastName" id="lastName" placeholder="Last Name" required>
+				<input type="text" name="username" id="username" placeholder="Username" required>
 				<input type="email" name="email" id="email" placeholder="Email" required>
 				<input type="email" name="confirmEmail" id="confirmEmail" placeholder="Confirm Email" required>
 				<input type="password" name="password" id="password" placeholder="Password" required>
-				<input type="password" name="confirmPassword" id="confirmPassword" placeholder="Confirm Password"
-					required>
+				<input type="password" name="confirmPassword" id="confirmPassword" placeholder="Confirm Password" required>
 				<input type="submit" name="submitButton" value="SUBMIT">
 			</form>
 			<a href="login.php" class="signInMessage">Already have an account? Sign in here!</a>
